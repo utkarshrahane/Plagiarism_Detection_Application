92a30a53cbda7942a755a402418732eb7ba1ac18
Utkarsh Rahane <rutkarsh99@gmail.com>
2022-09-16 12:03:49 +0530

    Search Engine change
    Increased code efficiency and aligned code with Google search engine.

--- websearch.py
+++ websearch.py
@@ -10,17 +10,40 @@ warnings.filterwarnings("ignore", module='bs4')
 def searchBing(query, num):
 
     #URL strutcure for searching over Microsoft Bing search engine.
-    url = 'https://www.bing.com/search?q=' + query
+    
     urls = []
 
-    page = requests.get(url, headers = {'User-agent': 'John Doe'})
+    # url2 = 'https://www.bing.com/search?q=' + query
+    # page = requests.get(url2, headers = {'User-agent': 'John Doe'})
+    # soup = bs(page.text, 'html.parser')
+
+    # for link in soup.find_all('a'):
+    #     url2 = str(link.get('href'))
+    #     if url2.startswith('http'):
+    #         if not url2.startswith('https://go.m') and not url2.startswith('https://go.m'):
+    #             urls.append(url2)
+
+    url1 = 'https://www.google.com/search?q=' + query
+    page = requests.get(url1, headers = {'User-agent': 'Mighty Near'})
     soup = bs(page.text, 'html.parser')
 
     for link in soup.find_all('a'):
-        url = str(link.get('href'))
-        if url.startswith('http'):
-            if not url.startswith('http://go.m') and not url.startswith('https://go.m'):
-                urls.append(url)
+        url1 = str(link.get('href'))
+        if url1.startswith('http'):
+            if not url1.startswith('https://go.m') and not url1.startswith('https://go.m'):
+                urls.append(url1)
+
+    
+    #urls = []
+
+    # page = requests.get(url2, headers = {'User-agent': 'John Doe'})
+    # soup = bs(page.text, 'html.parser')
+
+    # for link in soup.find_all('a'):
+    #     url2 = str(link.get('href'))
+    #     if url2.startswith('http'):
+    #         if not url2.startswith('https://go.m') and not url2.startswith('https://go.m'):
+    #             urls.append(url2)
     
     return urls[:num]
 
